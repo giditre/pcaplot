732710a158f703be04bd9eb2598cadc2a9ac346c
Gianluca Davoli <giditre@gmail.com>
2023-03-07 23:14:23 +0000

    Improved processing and plotting

--- tools/pcaplot.py
+++ tools/pcaplot.py
@@ -1,68 +1,112 @@
 import pathlib
+import sys
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import scapy.all as scapy
 
+# define name for processed data file, useful for caching processed data
+# NOTE: if a file with this name is found, the pcap files will not be processed
 data_file = pathlib.Path(__file__).parent.joinpath("tmp_df.csv")
 
 if data_file.exists():
+    # load data from existing file
     with open(str(data_file), newline="") as f:
-        df = pd.read_csv(f, index_col="time")
+        overall_df = pd.read_csv(f, index_col="time")
 else:
-
-    # pcap_file_name_list = [""]
-
-    # pcap_flow = scapy.PcapReader("../sample_pcap_files/R11_R21.pcap")
-    # pcap_flow = list(pcap_flow)
-
-    # pcap_flow = scapy.rdpcap("../sample_pcap_files/R11_R21.pcap")
-
+    # load pcap file as packet lists
     pcap_flows: dict[str, scapy.PacketList] = {
         file_path.stem: scapy.rdpcap(str(file_path))
         for file_path in pathlib.Path(__file__)
-        .parent.parent.joinpath("sample_pcap_files")
+        .parent.parent.joinpath("sample_pcap_files", "test01")
         .glob("*.pcap")
     }
 
-    print(pcap_flows)
+    # check loaded packet lists
+    print(pcap_flows, end="\n\n")
 
-    df: pd.DataFrame = pd.DataFrame(
-        [
-            (
-                name,
-                int(p.time),
-                len(p.getlayer(scapy.UDP)),
-            )
-            for name, packet_list in pcap_flows.items()
-            for p in packet_list
-            if p.haslayer(scapy.UDP)
-        ],
-        columns=["name", "time", "size"],
-    )
-    # sort packets by time
-    df.sort_values(by=["time"], inplace=True)
-    # refer time to that of first packet
-    df["time"] = df["time"] - df["time"].min()
-    # set time as index
-    df.set_index("time", inplace=True)
-    # compute data rate by grouping rows by time, summing the sizes
-    df = df.groupby(df.index).aggregate({"name": "first", "size": "sum"})
+    # define list of tables (DataFrames)
+    df_list: list[pd.DataFrame] = []
+
+    for name, packet_list in pcap_flows.items():
+
+        print(f"Process capture file {name}")
+
+        # build table (DataFrame) with a row per packet
+        # each row contains packet capture time and payload size
+        pcap_df: pd.DataFrame = pd.DataFrame(
+            [
+                (
+                    int(p.time),  # rounding (by truncation) to integer
+                    len(p.getlayer(scapy.UDP).payload) * 8,  # in bits
+                )
+                for p in packet_list
+                if p.haslayer(scapy.UDP)
+            ],
+            columns=["time", name],
+        )
+
+        # sort packets by time
+        pcap_df.sort_values(by=["time"], inplace=True)
+
+        # compute data rate by grouping rows by time, summing the sizes
+        pcap_df = pcap_df.groupby(pcap_df["time"]).aggregate({"time": "first", name: sum})
+
+        # normalize (choose appropriate method)
+        # TODO only normalize if required
+        pcap_df[name] = pcap_df[name].div(pcap_df[name].max())
+        # pcap_df[name] = (pcap_df[name] - pcap_df[name].mean()) / pcap_df[name].std()
+        # pcap_df[name] = (pcap_df[name] - pcap_df[name].min()) / (
+        #     pcap_df[name].max() - pcap_df[name].min()
+        # )
+
+        # set time as index
+        pcap_df.set_index("time", inplace=True)
+
+        # check table for this pcap
+        print(pcap_df, end="\n\n")
+
+        # append table to list
+        df_list.append(pcap_df)
+
+    # sort tables by size so that the first one is the longest
+    df_list.sort(key=lambda x: x.size, reverse=True)
+
+    # join the tables starting from the longest one
+    overall_df = df_list[0].copy().join(df_list[1:]).fillna(0)
+
+    # check overall table
+    print(overall_df, end="\n\n")
+
+    # offset time to that of the first packet (minimum time)
+    overall_df.index = overall_df.index - overall_df.index.min()
+
     # save DataFrame to file
-    df.to_csv(data_file)
+    overall_df.to_csv(data_file)
 
-print(df)
+# check table to be plotted
+print(overall_df)
 
-fig, ax = plt.subplots()
+# define figure
+fig, fig_ax = plt.subplots()
 
-df = df.groupby("name")["size"].plot.line(
-    y="size", marker=".", use_index=True, xlabel="Time [s]", ylabel="Data rate [B/s]", legend=True
+# plot lines
+plot_ax = overall_df.plot.line(
+    use_index=True, xlabel="Time [s]", ylabel="Data rate [normalized to 1]", legend=False
 )
-# print(df)
 
-# df.plot(y="size")
+# adjust markers and line styles
+markers = [".", "+", "x", "o", "v", "^", "<", ">", "8", "s", "p", "*", "h", "H", "D", "d", "P", "X"]
+linestyles = ["-", "--", ":", "-.", "-", "--", "-.", ":", "-", "--", "-.", ":"]
+for line, marker, linestyle in zip(plot_ax.get_lines(), markers, linestyles):
+    line.set_marker(marker)
+    line.set_linestyle(linestyle)
 
+# show the legend
+plot_ax.legend()
+
+# save the figure as pdf
 plt.savefig(str(pathlib.Path(__file__).parent.joinpath("tmp_plot.pdf")), bbox_inches="tight")
 
 # TODO if __name__ == "__main__"
-# TODO argparse: input and output file name(s), payload layer to be considered
+# TODO argparse: input and output file name(s), filters, payload to be considered
